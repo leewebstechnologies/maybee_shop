2678efe8a062e5cc6e0a2deeaad097b67ef553a7
leewebstechnologies <aliuajibade317@gmail.com>
2022-11-08 22:04:30 -0800

    maybee_shop

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Navbar = () => {
+    const Container = styled
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
